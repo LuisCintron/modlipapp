3422f14c5093bb950c6a3db4f41a360b8bf0acb6
Luis_Cintron <l.cintron9966@outlook.com>
2022-09-15 16:59:42 -0400

    did heading and sub heading and also style background

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div className='backgroundSkew'>
+          <div className='madlip-heading'>
+            <h1> My madlip Appication</h1>
+            <div className='madlip-subheading'>
+              Fill out the fields below and click generate button<br /> to see the Mod Lip story.
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
