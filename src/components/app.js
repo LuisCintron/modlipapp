0e6711567fc0b4623c7acfff5ebeba38086f6886
lou <l.cintron9966@gmail.com>
2022-09-19 21:03:45 -0400

    made mad lip component

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MadlipForm from './madlip_form';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
           Fill out the fields below and click the generate button <br/> to see the madlip storey
         </div>
         </div>
+        <MadlipForm />
         </div>
         
       </div>
---
+++ src/components/madlip_form.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+
+class MadlipForm  extends Component {
+    render() {
+        return <h1> Bottega Mad Lips</h1>
+    }
+}
+ export default MadlipForm
