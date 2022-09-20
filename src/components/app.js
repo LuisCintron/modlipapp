5955044a1f29880851245a6bbe02c7de6bbfc8ad
lou <l.cintron9966@gmail.com>
2022-09-19 20:04:24 -0400

    added header and header styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,17 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <div className='backgroundSkew'>
+        <div className='madlip-heading'>
         <h1>React Modlip App</h1>
+        <div className='madlip-subheading'>
+          Fill out the fields below and click the generate button <br/> to see the madlip storey
+        </div>
+        </div>
+        </div>
         
       </div>
+
     );
   }
 }
